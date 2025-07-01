2a8b3ae7af15601287c36d6fe87ea2a8a661f3e4
Harsha-vard <harshavardhanj369@gmail.com>
2025-07-01 15:03:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Quiz questions data
+const quizQuestions = [
+    {
+        question: "What does API stand for?",
+        options: ["Application Programming Interface", ...],
+        answer: 0
+    },
+    ...
+];
